319fa421d90af39ceb90716b9eeb5676e366c51a
ciekawypl <77553268+ciekawypl@users.noreply.github.com>
2024-11-12 14:43:27 +0100

    usun print

--- player/player.gd
+++ player/player.gd
@@ -8,7 +8,6 @@ var jump = 600
 var gravity = 30
 
 func _physics_process(_delta: float) -> void:
-	print(coins)
 	velocity.x = Input.get_axis("left", "right") * speed
 	
 	if Input.is_action_pressed("up") and is_on_floor():
